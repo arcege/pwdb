44f40dc920b2e0900ba161e99b457b3a072439bd
reillym <reillym@f02260e5-aad7-42fc-9c35-97c0d6f74615>
2009-11-30 12:08:12 +0000

    Changed to a paginator closer to 'less'; move paginator 'yesorno' functionality external to any class. Add 'debug' command to see and/or modify internals. Allow untagged entries to be referenced by '-' symbol. Fix bug in gen_password if default length is given or length is less then 6. Allow alternate database file.

--- cli.py
+++ cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
+import getopt
 import os
+import sys
 from cmd import Cmd
 from database import Database
 
@@ -10,6 +12,215 @@ DB_Filename = 'passwordy.db'
 __all__ = [
   'PwdbCmd'
 ]
+
+def YorN(prompt, reqresp=True, allowintr=False):
+    resp = ''
+    import sys, termios
+    real_prompt = '%s [y/N]' % prompt
+    infd = sys.stdin.fileno()
+    outfd = sys.stdout.fileno()
+    old = termios.tcgetattr(infd)
+    new = old[:]
+    new[3] &= ~(termios.ECHO|termios.ICANON)
+    try:
+        termios.tcsetattr(infd, termios.TCSADRAIN, new)
+        os.write(outfd, real_prompt)
+        while not resp:
+            ch = os.read(infd, 1)
+            if allowintr and ch == '\003': raise KeyboardInterrupt
+            if allowintr and ch == '\004': raise EOFError
+            if ch.lower() == 'y': resp = 'y'
+            elif ch.lower() == 'n': resp = 'n'
+            elif not reqresp: break
+    finally:
+        termios.tcsetattr(outfd, termios.TCSADRAIN, old)
+        os.write(outfd, resp)
+        os.write(outfd, '\n')
+    return resp == 'y'
+
+class Paginator:
+    def __init__(self, lines, input=None, output=None):
+        self.oldtermio = self.newtermio = None
+        import sys, termios
+        self.textlines = lines
+        self.pos = 0
+        self.lines = self.get_lines()
+        if input:
+            self.infd = input.fileno()
+        else:
+            self.infd = sys.stdin.fileno()
+        if output:
+            self.outfd = output.fileno()
+        else:
+            self.outfd = sys.stdout.fileno()
+        self.tcsetattr = termios.tcsetattr
+        self.TCSADRAIN = termios.TCSADRAIN
+        self.oldtermio = termios.tcgetattr(self.infd)
+        self.newtermio = self.oldtermio[:]
+        self.newtermio[3] &= ~(termios.ECHO | termios.ICANON)
+        self.clrscn = os.popen('tput clear', 'r').readline()
+    def __del__(self):
+        self.reset()
+    def set(self):
+        if self.newtermio is not None:
+            self.tcsetattr(self.infd, self.TCSADRAIN, self.newtermio)
+    def reset(self):
+        if self.oldtermio is not None:
+            self.tcsetattr(self.infd, self.TCSADRAIN, self.oldtermio)
+        self.oldtermio = None
+    def run(self):
+        done = False
+        try:
+            self.set()
+            while not done:
+                self.clear_screen()
+                self.display()
+                ch = self.more()
+                if ch == 'q':
+                    done = True
+                elif ch == ' ': # space
+                    self.pos = min(len(self.textlines), self.pos + self.lines)
+                elif ch == 'j' or ch == '\n': # carriage return
+                    self.pos = min(len(self.textlines), self.pos + 1)
+                elif ch == 'b':
+                    self.pos = max(0, self.pos - self.lines)
+                elif ch == 'k':
+                    self.pos = max(0, self.pos - 1)
+                elif ch == 'g':
+                    self.pos = 0
+                elif ch == 'G':
+                    self.pos = len(self.textlines) - self.lines
+                elif ch == '\003': # ctrl-C
+                    raise KeyboardInterrupt
+                elif ch == '\004': # ctrl-D
+                    raise EOFError
+        finally:
+            self.reset()
+    def clear_screen(self):
+        os.write(self.outfd, self.clrscn)
+    def display(self):
+        diff = 0
+        tl = len(self.textlines)
+        start = self.pos
+        end = min(tl, self.pos + self.lines - 1)
+        if end > tl:
+            diff = tl - end
+            start -= diff
+            end = tl
+        #print 'pos =', self.pos, 'textlines =', tl,
+        #print 'start =', start, 'end =', end, 'diff =', diff
+        for i in xrange(start, end):
+            print self.textlines[i]
+    def more(self):
+        os.write(self.outfd, 'More>')
+        ch = os.read(self.infd, 1)
+        os.write(self.outfd, '\r     \r')
+        return ch
+    def get_lines(cls):
+        try:
+            return cls.lines
+        except AttributeError:
+            try:
+                cls.lines = int(os.environ['LINES'])
+            except KeyError:
+                f = os.popen('stty size', 'r')
+                r, c = f.readline().strip().split()
+                cls.lines = int(r)
+                f.close()
+            return cls.lines
+    get_lines = classmethod(get_lines)
+
+class DebugCmd(Cmd):
+    prompt = 'debug> '
+    def __init__(self, db, *args, **kws):
+        Cmd.__init__(self, *args, **kws)
+        self.db = db
+
+    def do_list(self, argstr):
+        try:
+            self.db.open()
+            if argstr:
+                entries = [
+                    '%s\t%s' % (e.uid, e.name) for e in self.db
+                        if e == argstr
+                ]
+            else:
+                entries = [
+                    '%s\t%s' % (e.uid, e.name) for e in self.db
+                ]
+        finally:
+            self.db.close()
+        try:
+            Paginator(entries).run()
+        except (EOFError, KeyboardInterrupt):
+            print
+    def help_list(self):
+        print 'list entries with uids'
+    def do_show(self, argstr):
+        args = argstr.split()
+        try:
+            self.db.open()
+            for entry in self.db:
+                for arg in args:
+                    if arg[:1] == '#':
+                        if entry.uid == arg[1:]:
+                            self.display_entry(entry)
+                    elif entry.name == arg:
+                        self.display_entry(entry)
+        finally:
+            self.db.close()
+    def help_show(self):
+        print 'display details of an uid or named entry'
+    def display_entry(self, entry):
+        print 'uid: #%s' % entry.uid
+        print 'mtime: %s' % entry.mtime
+        for field in ('name', 'acct', 'pswd', 'url', 'label'):
+            val = getattr(entry, field)
+            print '%s: %s' % (entry.fieldnames[field], val)
+        print '%s:\t%s' % (
+            entry.fieldnames['notes'], entry.notes.replace('\n', '\n\t')
+        )
+    def do_mtime(self, argstr):
+        args = argstr.split()
+        if len(args) != 2:
+            print 'mtime requires to arguments: uid mtime'
+            return False
+        uid, mtime = tuple(args)
+        if uid[:1] == '#':
+            uid = uid[1:]
+        from database import Date
+        try:
+            mtime = Date(mtime)
+        except ValueError:
+            print 'mtime should be YYYYDDMM.HHMMSS format'
+            return False
+        try:
+            self.db.open()
+            entry = None
+            for e in self.db:
+                if e.uid == uid:
+                    entry = e
+                    break
+            else:
+                print 'uid not found'
+            entry.mtime = mtime
+            self.db.update(True)
+        finally:
+            self.db.close()
+    def help_mtime(self):
+        print 'set the mtime of an entry'
+
+    def do_EOF(self, argstr):
+        print
+        return True
+    def do_return(self, args):
+        return True
+    def help_return(self, args):
+        return 'Exit the debug section'
+    def do_quit(self, args):
+        return True
+    def help_quit(self):
+        print 'Exit the tag management section'
 
 class TagCmd(Cmd):
     prompt = 'tag> '
@@ -28,12 +239,18 @@ class TagCmd(Cmd):
                         tags.append(tag)
         finally:
             self.db.close()
+        if '' in tags:
+            p = tags.index('')
+            tags[p] = '-'
         print '\n'.join(sorted(tags))
     def help_list(self):
         print 'Display all tags'
 
     def do_show(self, argstr):
         args = argstr.split()
+        if '-' in args:
+            p = args.index('-')
+            args[p] = ''
         try:
             self.db.open()
             for entry in self.db:
@@ -50,6 +267,8 @@ class TagCmd(Cmd):
     def do_add(self, argstr):
         args = argstr.split()
         name = args[0]
+        if name == '-':
+            name = ''
         del args[0]
         try:
             self.db.open()
@@ -76,6 +295,8 @@ class TagCmd(Cmd):
                 tagset = entry.label.split(',')
                 ch = False
                 for arg in args:
+                    if arg == '-':
+                        arg = ''
                     if arg in tagset:
                         tagset.remove(arg)
                         tagset.sort()
@@ -96,13 +317,17 @@ class TagCmd(Cmd):
             print 'tag rename requires two arguments'
             return
         oldtag, newtag = args
+        if oldtag == '-':
+            oldtag = ''
+        if newtag == '-':
+            newtag = ''
         try:
             self.db.open()
             force = False
             for entry in self.db:
                 tagset = entry.label.split(',')
                 for i in xrange(len(tagset)):
-                    print 'tagset[%d] = %s' % (i, repr(tagset[i]))
+                    #print 'tagset[%d] = %s' % (i, repr(tagset[i]))
                     if tagset[i] == oldtag:
                         tagset[i] = newtag
                         tagset.sort()
@@ -146,20 +371,14 @@ System to view and manupulate passwords and their metadata.'''
         args = argstr.split()
         try:
             self.db.open()
-            entries = list(self.db)
+            if argstr:
+                entrynames = [ e.name for e in self.db if e == argstr ]
+            else:
+                entrynames = [ e.name for e in self.db ]
         finally:
             self.db.close()
-        count = 0
         try:
-            for entry in entries:
-                if args:
-                    if args[0] in entry:
-                        print entry.name
-                        count += 1
-                else:
-                    print entry.name
-                    count += 1
-                count = self.paginator(count)
+            Paginator(entrynames).run()
         except (EOFError, KeyboardInterrupt):
             print
     def help_list(self):
@@ -237,7 +456,7 @@ System to view and manupulate passwords and their metadata.'''
             for arg in args:
                 entry = self.db.find(arg)
                 if entry is not None:
-                    if self.yesorno('Remove %s' % entry.name):
+                    if YorN('Remove %s' % entry.name):
                         del self.db[entry]
                 else:
                     print arg, 'not found'
@@ -289,6 +508,13 @@ System to view and manupulate passwords and their metadata.'''
     def help_tag(tags):
         print 'tag management'
 
+    def do_debug(self, argstr):
+        cli = DebugCmd(self.db)
+        if not argstr:
+            cli.cmdloop()
+        else:
+            cli.onecmd(argstr)
+
     def do_EOF(self, args):
         print
         return True
@@ -330,13 +556,12 @@ System to view and manupulate passwords and their metadata.'''
             new_label = label or entry.label
             new_notes = notes or entry.notes
             print 'Verify...'
-            print '%s:' % entry.fieldnames['name'], new_name
-            print '%s:' % entry.fieldnames['acct'], new_acct
-            print '%s:' % entry.fieldnames['pswd'], new_pswd
-            print '%s:' % entry.fieldnames['url'], new_url
-            print '%s:' % entry.fieldnames['label'], new_label
-            print '%s:' % entry.fieldnames['notes'], new_notes
-            if self.yesorno('Is this correct?'):
+            for name, value in (
+                ('name', new_name), ('acct', new_acct), ('pswd', new_pswd),
+                ('url', new_url), ('label', new_label)):
+                print '%s: ' % entry.fieldnames[name], value
+            print 'notes:\t%s' % new_notes
+            if YorN('Is this correct?'):
                 entry.name  = new_name
                 entry.acct  = new_acct
                 entry.pswd  = new_pswd
@@ -349,15 +574,25 @@ System to view and manupulate passwords and their metadata.'''
 
     def gen_password(self):
         password = ''
-        length = raw_input('Length? [10] ')
+        done = False
+        while not done:
+            length = raw_input('Length? [10] ')
+            try:
+                if length != '' and int(length) <= 6:
+                    print 'must be greater than 6 characters'
+                else:
+                    done = True
+            except ValueError:
+                pass
         if not length:
             length = '10'
-        symbols = self.yesorno('Symbols?', resprequired=False, allowintr=True)
-        cmd = 'newpasswd --number=%s' % length
+        symbols = YorN('Symbols?', reqresp=False, allowintr=True)
+        cmd = 'newpasswd --quiet --number=%s' % length
         if symbols:
             cmd = cmd + ' --symbols'
+        cmd += ' 2>/dev/null'
         while True:
-            f = os.popen(cmd + ' 2>/dev/null', 'r')
+            f = os.popen(cmd, 'r')
             line = f.readline()
             rc = f.close()
             if rc:
@@ -365,50 +600,27 @@ System to view and manupulate passwords and their metadata.'''
                 raise KeyboardInterrupt  # caught in the caller
             (strength, password) = line.rstrip().split('\t', 1)
             print line.rstrip()
-            if self.yesorno('Is this acceptable?', allowintr=True):
+            if YorN('Is this acceptable?', allowintr=True):
                 break
         return password
 
-    def yesorno(self, prompt, resprequired=True, allowintr=False):
-        resp = ''
-        if allowintr:
-            exceptions = (RuntimeError,)
-        else:
-            exceptions = (EOFError, KeyboardInterrupt)
-        while not resp:
-            try:
-                resp = raw_input('%s [y/N]' % prompt)
-            except exceptions:
-                print
-                return False
-            else:
-                if not resp and resprequired:
-                    continue
-                else:
-                    return (resp[:1].lower() == 'y')
-        return False
-
-    def paginator(self, count):
-        lines = self.get_lines()
-        #print 'paginator(count=%d, lines=%d)' % (count, lines)
-        if count < lines-1:
-            return count
-        raw_input('Type ENTER for more>')
-        return 0
-    def get_lines(self):
-        try:
-            return self.lines
-        except AttributeError:
-            try:
-                self.lines = int(os.environ['LINES'])
-            except KeyError:
-                f = os.popen('stty size', 'r')
-                r, c = f.readline().strip().split()
-                self.lines = int(r)
-                f.close()
-            return self.lines
-
 if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'f:h',
+                                    ['file=', 'help'])
+    except getopt.error, e:
+        raise SystemExit(e)
+    for opt, val in opts:
+        if opt == '--':
+            break
+        elif opt in ('-h', '--help'):
+            print sys.argv[0], '[opts]'
+            print '  -h|--help'
+            print '  -f|--file=dbfile'
+            raise SystemExit
+        elif opt in ('-f', '--file'):
+            DB_Filename = val
+
     try:
         cli = PwdbCmd()
         cli.cmdloop()
